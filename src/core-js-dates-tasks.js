8f5f5dc3066947a7ed4f15cfb6d926d635796b5b
Alv0425 <vilejshikova@gmail.com>
2024-03-24 01:51:22 +0300

    fix: update solution for getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -33,7 +33,11 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  return date.toLocaleTimeString();
+  return date.toLocaleTimeString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
 }
 
 /**
